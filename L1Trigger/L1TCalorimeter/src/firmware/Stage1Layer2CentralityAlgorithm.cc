f67be3e5ba12c7bd3755e2e5816c4a938abc3439
ginnocen <ginnocen@cern.ch>
2014-07-12 15:39:29 +0200

    energy of HFEtplus and HFminus stored separately

--- L1Trigger/L1TCalorimeter/src/firmware/Stage1Layer2CentralityAlgorithm.cc
+++ L1Trigger/L1TCalorimeter/src/firmware/Stage1Layer2CentralityAlgorithm.cc
@@ -26,19 +26,35 @@ l1t::Stage1Layer2CentralityAlgorithm::~Stage1Layer2CentralityAlgorithm() {
 void l1t::Stage1Layer2CentralityAlgorithm::processEvent(const std::vector<l1t::CaloRegion> & regions,
 							const std::vector<l1t::CaloEmCand> & EMCands,
 							std::vector<l1t::CaloSpare> * spares) {
-
-  int sumET = 0;
-  int regionET=0;
+  
+  double sumET=0.;
+  double regionET=0.; 
+  
+  double sumETPlus=0.;
+  double sumETMinus=0.;
+  
+  double regionETPlus=0.;    
+  double regionETMinus=0.; 
 
   for(std::vector<CaloRegion>::const_iterator region = regions.begin(); region != regions.end(); region++) {
 
     if (region->hwEta() > 3 && region->hwEta() < 18) {
       continue;
     }
+    regionETPlus=0.;
+    regionETMinus=0.;
+    regionET=0.;
+    
+    if(region->hwEta() >= 18) regionETPlus=region->hwPt();
+    if(region->hwEta() <= 3) regionETMinus=region->hwPt();
     regionET=region->hwPt();
+    
+    sumETPlus +=regionETPlus;
+    sumETMinus +=regionETMinus; 
     sumET +=regionET;
-  }
+  } 
+  
   ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<double> > dummy(0,0,0,0);
-  l1t::CaloSpare centrality (*&dummy,CaloSpare::CaloSpareType::Centrality,sumET,0,0,0);
+  l1t::CaloSpare centrality (*&dummy,CaloSpare::CaloSpareType::Centrality,(int)sumETPlus,(int)sumETMinus,(int)sumET,0);
   spares->push_back(centrality);
 }
